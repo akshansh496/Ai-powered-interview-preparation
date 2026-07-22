0b4217ed9027be1c4f9e1848f97f2fbb66c00131
akshansh496 <guptaakshansh1@gmail.com>
2026-07-23 00:19:47 +0530

    login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -24,18 +24,18 @@ async function registerUserController(req,res){
 
     const hash=await bycrypt.hash(password,10);
 
-    const newUser=new userModel.create({
+    const newUser=await userModel.create({
         username,
         email,
         password:hash,
     })
 
     const token=jwt.sign(
-        {id:user._id,username:user.username},
+        {id:newUser._id,username:newUser.username},
         process.env.JWT_SECRET,
         {expiresIn:"1d"}
     )
-    res.cookie("token",token,)
+    res.cookie("token",token)
 
     res.status(201).json({
         message:"User registered successfully",
@@ -47,4 +47,40 @@ async function registerUserController(req,res){
     })
 }
 
-module.exports={registerUserController}
+/**
+ * @name loginUserController
+ * @description Login a user, expects email and password in the request body
+ * @access Public
+ */
+async function loginUserController(req,res){
+    const {email,password}=req.body
+
+    const user=await userModel.findOne({email})
+
+    if(!user){
+        return res.status(400).json({message:"User not found"})
+    }
+
+    const isPasswordValid =await bycrypt.compare(password,user.password)
+
+    if(!isPasswordValid){
+        return res.status(400).json({message:"User not found"})
+    }
+
+    const token=jwt.sign(
+        {id:user._id,username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn:"1d"}
+    )
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"User logged in successfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email,
+        },
+    })
+}
+module.exports={registerUserController,loginUserController}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,4 +9,13 @@ const authRouter=express.Router()
  */
 authRouter.post("/register",authController.registerUserController)
 
+
+/**
+ * @route POST  /api/auth/login
+ * @description Login User with email and password
+ * @access Public
+ */
+
+authRouter.post("/login",authController.loginUserController)
+
 module.exports=authRouter;
