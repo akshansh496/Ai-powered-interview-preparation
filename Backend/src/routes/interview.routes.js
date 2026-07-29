497d89e7f4170864370727b25216ef14a1ccf602
akshansh496 <guptaakshansh1@gmail.com>
2026-07-29 23:20:00 +0530

    ai api integrted

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -1,5 +1,15 @@
 const express=require("express")
+const authMiddleware=require("../middlewares/auth.middleware")
+const interviewController=require("../controllers/interview.controller")
+const upload=require("../middlewares/file.middleware")
 
 const interviewRouter=express.Router()
+
+/**
+ * @route POST  /api/interview
+ * @description Generate new Interview Report on the basis of user self description,resume pdf and job description
+ * @access 
+ */
+interviewRouter.post("/",authMiddleware.authUser,upload.single("resume"),interviewController.generateInterViewReportController)
 
 module.exports=interviewRouter
