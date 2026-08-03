8d54803f7017af37f79d1ff6800345d9229f253d
akshansh496 <guptaakshansh1@gmail.com>
2026-08-04 01:41:06 +0530

    resume download feature

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -26,4 +26,12 @@ interviewRouter.get("/report/:interviewId",authMiddleware.authUser,upload.single
  */
 interviewRouter.get("/",authMiddleware.authUser,upload.single("resume"),interviewController.getAllInterviewReportsController)
 
+/**
+ * @route GET /api/interview/resume/pdf
+ * @description generate resume pdf on the basis of user self description, resume content and job description.
+ * @access private
+ */
+interviewRouter.post("/resume/pdf/:interviewReportId", authMiddleware.authUser, interviewController.generateResumePdfController)
+
+
 module.exports=interviewRouter
